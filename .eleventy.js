5c3318cfafab57ad649e9ff6316b1570be61cec8
Visionmsprod <luiscarlosmueses@gmail.com>
2025-06-12 12:05:16 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function(eleventyConfig) {
+  // Esta línea le dice a Eleventy que copie todos estos archivos/carpetas
+  // desde 'src' directamente a la carpeta final del sitio.
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/animations.css");
@@ -6,12 +8,17 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/luisk.jpg");
   eleventyConfig.addPassthroughCopy("src/favicon.png");
 
+  // Esta es la configuración corregida.
+  // Le dice a Eleventy dónde encontrar todo DENTRO de la carpeta 'src'.
   return {
     dir: {
       input: "src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
+      includes: "_includes", // La carpeta de plantillas está en 'src/_includes'
+      data: "_data",       // La carpeta de datos está en 'src/_data'
+      output: "_site"      // La carpeta donde se generará el sitio final
+    },
+    // Estas dos líneas son importantes para que los archivos .njk y .html se procesen correctamente.
+    templateFormats: ["njk", "md", "html"],
+    htmlTemplateEngine: "njk"
   };
 };
