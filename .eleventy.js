6dfabb24d2a5827443dd73b95519031e40e6e899
Visionmsprod <luiscarlosmueses@gmail.com>
2025-06-13 14:42:40 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,41 @@
 module.exports = function(eleventyConfig) {
+  
+  // --- FILTRO PERSONALIZADO PARA EL COTIZADOR ---
+  // Esta función le enseña a Eleventy qué significa "filterby" para que el cotizador funcione.
+  eleventyConfig.addFilter("filterby", (collection, key, value) => {
+    if (!collection) {
+      return [];
+    }
+    return collection.filter(item => {
+      const data = item.data;
+      if (data && data[key] !== undefined) {
+        return data[key] === value;
+      }
+      return false;
+    });
+  });
+
+  // --- COLECCIÓN PERSONALIZADA PARA EL PORTAFOLIO ---
+  eleventyConfig.addCollection("portafolio", function(collectionApi) {
+    return collectionApi.getFilteredByTag("portafolio");
+  });
+
+  // --- COLECCIÓN PERSONALIZADA PARA LOS SERVICIOS DEL COTIZADOR ---
+  eleventyConfig.addCollection("servicios_cotizador", function(collectionApi) {
+    return collectionApi.getFilteredByTag("servicios_cotizador");
+  });
+  
+  // --- COPIAR ARCHIVOS ESTÁTICOS ---
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy({"src/_redirects": "_redirects"});
   eleventyConfig.addPassthroughCopy("src/logo1.png");
   eleventyConfig.addPassthroughCopy("src/luisk.jpg");
   eleventyConfig.addPassthroughCopy("src/favicon.png");
-  eleventyConfig.addPassthroughCopy({"src/_redirects": "_redirects"});
+
+  // --- CONFIGURACIÓN DE DIRECTORIOS DE ELEVENTY ---
   return {
     dir: {
       input: "src",
@@ -14,7 +43,6 @@ module.exports = function(eleventyConfig) {
       data: "_data",
       output: "_site"
     },
-    templateFormats: ["njk", "md", "html"],
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk"
   };
